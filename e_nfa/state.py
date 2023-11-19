0cce156d3a2eb1fa3ddf3dc2fef97e7365bbeb4e
samuelhobbs <90936990+samuelhobbs@users.noreply.github.com>
2023-11-18 22:23:11 -0800

    start on nfa
    This is just some ideas on the board for epsilon NFA stuff

---
+++ e_nfa/state.py
@@ -0,0 +1,21 @@
+from typing import List, Optional
+from regexp.tokens import LiteralToken
+
+class StateToken:
+    __transitions: list                     #This is a list of list[(LiteralToken, StateToken)]
+    
+    def __init__(self):
+        self.__transitions = []
+    
+    def addTransition(self, tValue:LiteralToken, destination):  #addTransition(self, LiteralToken, StateToken)
+        self.__transitions.append((tValue, destination))
+        
+    def searchTransitionDestination(self, tValue:LiteralToken) -> Optional[list]:
+        destinations = []
+        
+        indicies = iter(range(len(self.__transitions)))
+        for i in indicies:
+            if self.__transitions[i][0] == tValue:
+                destinations.append(self.__transitions[i][1])
+                
+        return None if len(destinations) == 0 else destinations
